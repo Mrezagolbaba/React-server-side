346ce358fa96452957c73fe4c3ff98ab15a2a419
Mohamadreza golbaba <m.reza.golbaba@gmail.com>
2021-01-18 10:02:14 +0330

    update server.js

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ import React from 'react';
 import ReactDomServer from 'react-dom/server';
 import {StaticRouter} from 'react-router';
 import bodyParser from 'body-parser';
+import {Helmet} from 'react-helmet'
 
-import {App} from './src/app'
+import App from './src/app'
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json())
-app.use(express.static('build'))
+app.use(express.static('build/public'))
 
 app.get('*', (req,res)=>{
 
@@ -22,16 +23,18 @@ app.get('*', (req,res)=>{
             <App/>
         </StaticRouter>
     )
-
+    const helmet = Helmet.renderStatic()
     const html =`
         <html>
             <head>
+                ${helmet.meta.toString()}
+                ${helmet.title.toString()}
             </head>
-            
             <body>
                 <div id="root">
                     ${content}
                 </div>
+                <script src="client_bundle.js"></script>
             </body>
         </html>
 
